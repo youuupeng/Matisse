6bc60996d09e0359c832b8835718ca66ed717b45
peng.you <christopher.you@royole.com>
2019-09-05 09:57:35 +0800

    #Kotlin temp commit

--- matisse/src/main/java/com/zhihu/matisse/internal/utils/Util.kt
+++ matisse/src/main/java/com/zhihu/matisse/internal/utils/Util.kt
@@ -7,6 +7,11 @@ package com.zhihu.matisse.internal.utils
  * @author peyo
  * @date 9/4/2019
  */
+val Any.TAG: String
+    get() {
+        val tag = javaClass.simpleName
+        return if (tag.length <= 23) tag else tag.substring(0, 23)
+    }
 
 fun failIllegalArgument(message: String): Nothing = throw IllegalArgumentException(message)
 
